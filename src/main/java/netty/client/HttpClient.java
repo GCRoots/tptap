b715d5e92fd2715f18b9a951bf41a3c25d894584
GC <1007879951@qq.com>
2019-07-30 22:35:01 +0800

    Initial commit

---
+++ src/main/java/netty/client/HttpClient.java
@@ -0,0 +1,4 @@
+package netty.client;
+
+public class HttpClient {
+}
---
+++ src/main/java/netty/server/HttpInitializer.java
@@ -0,0 +1,20 @@
+package netty.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class HttpInitializer extends ChannelInitializer<SocketChannel> {
+
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        pipeline.addLast(new HttpResponseEncoder());
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new HttpObjectAggregator(65535));
+        pipeline.addLast(new ChunkedWriteHandler());
+    }
+}
